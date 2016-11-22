045924863e8b42b8cd5087e59fdb5f74d0fecbde
Preston Moore <prestonkmoore@gmail.com>
2016-11-21 21:19:32 -0800

    verifier checks committer name

--- final_product/verify_bsl.py
+++ final_product/verify_bsl.py
@@ -1,5 +1,19 @@
 import sys
+import json
 
 if __name__ == '__main__':
-  print 'Verification complete'
+  c = open('allowed_committers.json', 'r')
+  data = c.read()
+  c.close()
+  committers = json.loads(data)
+  f =  open('bsl.json', 'r')
+  data = f.read()
+  f.close()
+  data = json.loads(data)
+  for idx, i in enumerate(data):
+    r = json.loads(i)
+    if r['committer_name'] not in committers['allowed_committers']:
+      print('ERROR: Committer name {} for commit {} is not in an allowed committer'
+            .format(r['committer_name'], idx))
+      sys.exit(1)
   sys.exit(0)
